3fe3ba331b022993b428922abe5ca558a3d373e1
Shamim Hossain <pearlygatescorp@gmail.com>
2023-11-07 00:13:26 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+<img src="appicon.png" width="100"></img>
+
+# Google Suggest in NodeJS
+
+<br>
+
+### Release
+Version: 1.0.0<br>
+On: 4 November, 2023
+
+<br>
+
+### Dependencies
+
+axios
+
+<br>
+
+## Instal Dependencies
+
+```
+npm i axios
+```
+
+
+
+## Developer
+
+<strong>Shamim Hossain</strong>
+
+<sparrowmtm@gmail.com>
+
+<https://sparrowtech.org>
+
+<https://viewbud.com>
+
+<https://www.upwork.com/freelancers/~019f659b0849b9115b>
+
+<https://www.youtube.com/@davidlee5497>
---
+++ app.js
@@ -0,0 +1,38 @@
+var axios = require('axios');
+var fs = require('fs');
+
+let link = 'https://www.google.com/complete/search?&hl=LANG&gl=LOC&q=QUERY&client=chrome';
+
+function GetGoogleSuggests(lang, loc, query){
+	if (!query) {
+		console.log('Query string is empty!');
+		return;
+	}
+
+	link = link.replace(/LANG/g, lang).replace(/LOC/g, loc).replace(/QUERY/g, encodeURIComponent(query));
+
+	axios.get(link)
+	.then(response => {
+		let formattedJson = JSON.stringify(response.data, null, 2);
+		fs.writeFileSync('output.json', formattedJson);
+
+		let items = response.data[1];
+		for (let i=0; i<items.length; i++){
+			console.log((i+1), ":", items[i]);
+		}
+	})
+	.catch(error => {
+		console.log(error);
+	});
+}
+
+GetGoogleSuggests('en', 'us', 'youtube');
+
+/*
+
+China		CN		Chinese			ZH	
+France		FR		French			FR
+India		IN		Hindi			HI
+Mexico		MX		Spanish			ES
+
+*/
